f4d1ef965cc4a5225c122d7a37c7c41d8370790a
chaitanya <vamsi.blaze1@gmail.com>
2026-03-25 12:34:27 +0530

    feature : added route tables and nat

---
+++ terraform/nat.tf
@@ -0,0 +1,15 @@
+# Elastic Ip for Nat
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  depends_on = [ aws_internet_gateway.main ]
+  tags = {Name = "${local.common_name}-nat-eip"}
+}
+
+# Nat gateway
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public.id
+  depends_on = [ aws_internet_gateway.main ]
+  tags = {Name="${local.common_name}-nat-gw"}
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -40,3 +40,28 @@ resource "aws_subnet" "private" {
     Name = "${local.common_name}-private-subnet"
   }
 }
+
+# Route tables
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route{
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags ={
+    Name = "${local.common_name}-public-rt"
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.main.id
+  }
+  tags = {
+    Name = "${local.common_name}-private-rt"
+  }
+}
